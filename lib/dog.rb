650eff3062e635857062816efa6758d8eb3833b5
Ted Barlow <ted@noukod.com>
2018-09-05 15:00:55 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+  def initialize(name, breed="Mutt")
+    @name = name
+    @breed = breed
+  end
+
+  def name
+    @name
+  end
+
+  def breed
+    @breed
+  end
+
+  def breed=(newbreed)
+    @breed = newbreed
+  end
+  
+end
